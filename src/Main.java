e5c67f1dda33a2ce45f81b8ef8669999a83f98de
NevaMefa <k0rbanan@yandex.ru>
2023-03-11 11:29:46 +0300

    work

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int gradtr = 11;// тут вот как-то надо индекс привзать
+        int davlpp = 5; //тут вот 2й индекс, но там априори 5 или 10
+int sistdla; //тут 3й нндекс сумма 1+2
+        if (gradtr > 0) {
+            sistdla = gradtr + davlpp;
+        } else {
+            sistdla = 0;
+        }
+        System.out.println("reult" + sistdla);
     }
 }
